e997da9f1e96ee646ae88f93d804348e20c107ac
mikhail <trushakin@list.ru>
2023-12-09 20:00:19 +0300

    работа с массивами 2

---
+++ ex010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array ={11,52,25,52,7,58,98,44,52,32,55,98,85,147};
+
+int n = array.Length;
+int find = 52;
+
+int index = 0;
+
+while(index< n)
+{
+    if(array[index] ==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
